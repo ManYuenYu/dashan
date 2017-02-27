8cd61c104975f8ceadd3b9e1b03fa9fff3d771ae
ManYuen <btcs00@qq.com>
2017-02-27 17:14:27 +0800

    项目初始化

---
+++ dashan/dashan/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  dashan
+//
+//  Created by ManYuen on 2/27/17.
+//  Copyright © 2017 Yuen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
